b217355a26e5b84c4b5ff604f1482512dd1665f9
abdisetiakawan <bedikadiryt@gmail.com>
2024-09-14 22:20:23 +0700

    "Added handler, notes, routes, and server files with initial implementation for note creation"

---
+++ src/routes.js
@@ -0,0 +1,10 @@
+const { addNoteHandler } = require('./handler');
+const routes = [
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: addNoteHandler,
+  },
+];
+
+module.exports = routes;
